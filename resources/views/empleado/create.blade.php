f74616b266f2c946ec15b73fec8c798d3de4097c
claualegrec <claudialegrec.3@outlook.es>
2022-05-23 01:10:29 -0600

    Creacion de formulrio para crear empleado

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,1 +1,48 @@
-Formulario de creacion de empleado
+<form action="" method="post" enctype="multipart/form-data">
+  <!-- Input para nombre del empleado -->
+  <div style="padding:10px">
+    <label for="Nombre">Nombre</label>
+    <br>
+    <input type="text" name="Nombre" id="Nombre"></input>
+  </div>
+
+  <br>
+
+  <!-- Input para apellido paterno del empleado -->
+  <div style="padding:10px">
+    <label for="ApellidoPaterno">Nombre</label>
+    <br>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno></input>
+  </div>
+
+  <br>
+
+  <!-- Input para apellido paterno del empleado -->
+  <div style="padding:10px">
+    <label for="ApellidoMaterno">Apellido Materno</label>
+    <br>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno"></input>
+  </div>
+
+  <br>
+
+  <!-- Input para correo del empleado -->
+  <div style="padding:10px">
+    <label for="Correo">Correo</label>
+    <br>
+    <input type="text" name="Correo" id="Correo"></input>
+  </div>
+
+  <br>
+
+  <!-- Input para foto del empleado -->
+  <div style="padding:10px">
+    <label for="Foto">Foto</label>
+    <br>
+    <input type="file" name="Foto" id="Foto"></input>
+  </div>
+
+  <br>
+
+  <input type="submit" name="Enviar" id="Enviar"></input>
+</form>
