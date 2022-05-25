9d04c36391748530c19b2c7702a70c9129e15b38
claualegrec <claudialegrec.3@outlook.es>
2022-05-24 21:18:43 -0600

    validacion de datos

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -3,11 +3,24 @@
   <div>
     <h1> {{ $modo }} empleado </h1>
   </div>
+
+  @if(count($errors)>0)
+
+    <div class="alert alert-danger" role="alert">
+      <ul>
+        @foreach( $errors->all() as $error )
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+
+  @endif
   
   <div style="padding:10px">
     <label for="Nombre">Nombre</label>
     <br>
-    <input type="text" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre"></input>
+    <input type="text" name="Nombre" 
+    value="{{ isset($empleado->Nombre)?$empleado->Nombre:old('Nombre') }}" id="Nombre"></input>
   </div>
 
   <br>
@@ -16,7 +29,8 @@
   <div style="padding:10px">
     <label for="ApellidoPaterno">Apellido Paterno</label>
     <br>
-    <input type="text" name="ApellidoPaterno" value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'' }}" id="ApellidoPaterno"></input>
+    <input type="text" name="ApellidoPaterno" 
+    value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:old('ApellidoPaterno') }}" id="ApellidoPaterno"></input>
   </div>
 
   <br>
@@ -25,7 +39,8 @@
   <div style="padding:10px">
     <label for="ApellidoMaterno">Apellido Materno</label>
     <br>
-    <input type="text" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'' }}" id="ApellidoMaterno"></input>
+    <input type="text" name="ApellidoMaterno" 
+    value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:old('ApellidoPaterno') }}" id="ApellidoMaterno"></input>
   </div>
 
   <br>
@@ -34,7 +49,8 @@
   <div style="padding:10px">
     <label for="Correo">Correo</label>
     <br>
-    <input type="text" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:'' }}" id="Correo"></input>
+    <input type="text" name="Correo" 
+    value="{{ isset($empleado->Correo)?$empleado->Correo:old('Correo') }}" id="Correo"></input>
   </div>
 
   <br>
@@ -44,7 +60,7 @@
     <label for="Foto">Foto</label>
     <br>
     @if(isset($empleado->Foto))
-    <img src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
+    <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
     @endif
     <input type="file" name="Foto" value="" id="Foto"></input>
   </div>
