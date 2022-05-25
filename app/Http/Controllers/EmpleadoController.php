e1790277ddbf29ad9b8763564947917f7b919fb2
claualegrec <claudialegrec.3@outlook.es>
2022-05-24 21:33:49 -0600

    Validación en edición de empleado

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -100,13 +100,29 @@ class EmpleadoController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $campos = [
+            'Nombre' => 'required|string|max:100',
+            'ApellidoPaterno' => 'required|string|max:100',
+            'ApellidoMaterno' => 'required|string|max:100',
+            'Correo' => 'required|email',
+        ];
+
+        $mensaje = [
+            'required' => 'El :attribute es requerido',
+        ];
+
+        if($request->hasFile('Foto')) {
+            $campos = [ 'Foto' => 'required|max:10000|mimes:jpeg,png,jpg' ];
+            $mensaje = [ 'Foto.required' => 'La foto es requerida' ];
+        }
+
+        $this->validate($request, $campos, $mensaje);
+
         $datosEmpleado = request()->except(['_token', '_method']);
 
         if($request->hasFile('Foto')) {
-            // Encuentra la foto anterior y la borra del public para después reemplazarla
             $empleado = Empleado::findOrFail($id);
             Storage::delete('public/'.$empleado->Foto);
-
             $datosEmpleado['Foto'] = $request->file('Foto')->store('uploads','public');
         }
 
